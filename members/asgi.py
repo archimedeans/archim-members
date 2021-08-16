f1d7aa0b0fbdf02a995fdcf00f36d07a41897831
Adam Kelly <adamkelly2201@gmail.com>
2021-08-16 01:37:10 +0100

    Change project name to members

--- members/asgi.py
+++ members/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for archimmembers project.
+ASGI config for members project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'archimmembers.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'members.settings')
 
 application = get_asgi_application()

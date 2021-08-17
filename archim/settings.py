d0d9e425323943cb2fe101c1b819e77d0ca24f77
Adam Kelly <adamkelly2201@gmail.com>
2021-08-17 08:28:29 +0100

    Add committee page

--- archim/settings.py
+++ archim/settings.py
@@ -17,6 +17,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'members.apps.MembersConfig',
+    'committee.apps.CommitteeConfig',
     'core.apps.CoreConfig',
     
     'django.contrib.admin',
--- archim/urls.py
+++ archim/urls.py
@@ -5,8 +5,9 @@ from django.urls import path, include
 
 urlpatterns = [
     path('members/', include('members.urls')),
-    path('admin/', admin.site.urls),
     path('', include('core.urls')),
+    path('', include('committee.urls')),
+    path('admin/', admin.site.urls)
 ]
 
 if settings.DEBUG:
